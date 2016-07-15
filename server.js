6f5912cc58bbc6517a3adf69985433893e98fed5
jason <appleoxxo@gmail.com>
2016-07-15 09:01:41 +0800

    port

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req,res){
-    res.send('Hello World');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
+app.listen(process.env.PORT ||8080);
 
-app.listen(process.env.port||8080);
+console.log(process.env.PORT ||8080);
