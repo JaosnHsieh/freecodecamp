716f27821697e6c4e4b62618900c7cac190bf35a
jasonHsieh <appleoxxo@gmail.com>
2016-07-15 09:32:48 +0800

    finished time server

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var app = express();
- 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var moment = require('moment');
+
+ app.get('/', function (req, res) {
+  res.sendFile(__dirname+'/public/index.html');
+  
 });
+app.get('/:datestring', function(req,res) {
+  var myDate;
+  if(/^\d{8,}$/.test(req.params.datestring)) {
+    myDate = moment(req.params.datestring, "X");
+  } else {
+    myDate = moment(req.params.datestring, "MMMM D, YYYY");
+  }
+
+  if(myDate.isValid()) {
+    res.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+
+
+});
+app.use('/public',express.static(__dirname+'/public'));
+
 app.listen(process.env.PORT ||8080);
 
 console.log(process.env.PORT ||8080);
